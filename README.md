34789e87c426dcf9a4b1b0be7b82c33285786abc
sid-r-singh <ofw3210@gmail.com>
2020-09-16 09:36:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# iot-tank
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/iot-tank)
---
+++ index.js
@@ -0,0 +1,15 @@
+// Import stylesheets
+import './style.css';
+// Write Javascript code!
+var cnt=document.getElementById("count"); 
+var water=document.getElementById("water");
+var percent=cnt.innerText;
+var interval;
+interval=setInterval(function(){ 
+  percent++; 
+  cnt.innerHTML = percent; 
+  water.style.transform='translate(0'+','+(100-percent)+'%)';
+  if(percent==60){
+    clearInterval(interval);
+  }
+},60);
